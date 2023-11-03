2489eaf88023297068126f385f76b6fcc0a0c7bf
Milan-George7 <146876687+Milan-George7@users.noreply.github.com>
2023-11-03 11:45:01 +0530

    netflix-clone

---
+++ javascript.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+        $(window).scroll(function(){
+            var scroll = $(window).scrollTop();
+            if(scroll > 100){
+                $(".netflix-navbar").css("background", "#0C0C0C")
+
+            }
+            else{
+                $(".netflix-navbar").css("background", "transparent")
+            }
+        })
+})
